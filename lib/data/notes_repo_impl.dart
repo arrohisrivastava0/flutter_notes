a6fcf5c0c7bff645e2a37d13f504242a0fa5e170
arrohisrivastava <arrohisrivastava6@gmail.com>
2024-12-03 00:43:30 +0530

    Add data layer

---
+++ lib/data/notes_repo_impl.dart
@@ -0,0 +1,30 @@
+import 'package:flutter_notes/domain/modal/notes_modal.dart';
+import 'package:flutter_notes/domain/repository/notes_repo.dart';
+import 'package:flutter_notes/services/database_service.dart';
+
+class NotesRepoImpl implements NotesRepo{
+  final DatabaseService _databaseService;
+
+  NotesRepoImpl(this._databaseService);
+
+  @override
+  Future<void> addNote(NotesModal notesModal) async {
+    await _databaseService.addNote(notesModal);
+  }
+
+  @override
+  Future<void> deleteNote(NotesModal notesModal) async {
+    await _databaseService.deleteNote(notesModal);
+  }
+
+  @override
+  Future<List<NotesModal>> getNotes() async{
+    return await _databaseService.getNotes();
+  }
+
+  @override
+  Future<void> updateNote(NotesModal notesModal) async{
+    await _databaseService.updateNote(notesModal);
+  }
+
+}
